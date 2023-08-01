c16458b11e2ad8f4ffe31d60c26853d71a4c68bb
dattlab <122553391+dattlab@users.noreply.github.com>
2023-08-01 21:27:20 +0800

    Add missing.csv; modify pcost for exec 1.31

--- intro/pcost.py
+++ intro/pcost.py
@@ -10,14 +10,17 @@ import gzip
 
 def purchase_cost(filename) -> None:
     # Uncomment to open the gzip file instead
-    # with gzip.open("data/portfolio.csv.gz", "rt") as list_stocks:
+    # with gzip.open(filename, "rt") as list_stocks:
     with open(filename) as list_stocks:
         total_cost = 0
 
         next(list_stocks)
         for stock in list_stocks:
-            _, num_shares, purchase_price = stock.split(",")
-            total_cost += (float(purchase_price) * int(num_shares))
+            try:
+                _, num_shares, purchase_price = stock.split(",")
+                total_cost += (float(purchase_price) * int(num_shares))
+            except ValueError:
+                print("There are some missing fields. Skipping them...")
 
     return total_cost 
 
